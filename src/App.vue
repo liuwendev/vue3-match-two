88a73553daec64913df089bdde31fddb2c1cebc5
Liu Wen <haba-haba555@ya.ru>
2022-11-06 01:48:12 +0300

    Prepared game template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Board />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import Board from '@/components/Board';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #cfd3dc;
+  background-color: #141414;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  user-select: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
 }
 </style>
---
+++ src/components/BoardItem.vue
@@ -0,0 +1,82 @@
+<template>
+    <div :class="
+        'board-item ' 
+        + ( props.selected ? 'selected' : '' ) 
+        + ( props.closed ? 'closed' : '' )
+    ">
+        <i v-if="iconId" :class="'icon icon-' + iconId"></i>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    iconId: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    selected: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    closed: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+});
+
+</script>
+
+<style>
+.board-item {
+    position: relative;
+    border: 2px solid #262727;
+    width: 96px;
+    height: 96px;
+    cursor: pointer;
+}
+
+.selected {
+    background-color: #f56c6c;
+}
+
+.closed {
+    background-color: #67c23a;
+}
+
+.icon {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+.icon-1,
+.icon-2,
+.icon-3,
+.icon-4 {
+    background: url('../assets/icons.png');
+    background-size: 300px 300px;
+}
+
+.icon-1 {
+    background-position: 0 0;
+}
+
+.icon-2 {
+    background-position: 100px 0;
+}
+
+.icon-3 {
+    background-position: 200px 0;
+}
+
+.icon-4 {
+    background-position: 100px 200px;
+}
+
+</style>
